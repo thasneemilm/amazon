9dd0ebb8c7c83748cb706e7b7dc4cd994918f282
thasneem <thasneemilm@gmail.com>
2024-04-15 23:59:13 +0530

    prodduct page  added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main> list products </main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={'/product/${product.slug}'}>
+                <img src={product.image} alt={product.slug} />
+              </a>
+              <div className="product-info">
+                <a href={'/product/${product.slug}'}>
+                  <p>{product.name}</p>
+                </a>
+                <p>{product.price}</p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-hirt',
+      category: 'shirts',
+      image: '/images/p1.jpg',
+      price: '120',
+      instock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+    },
+
+    {
+      name: 'Adidas Slim Shirt',
+      slug: 'Adidas-slim-hirt',
+      category: 'shirts',
+      image: '/images/p2.jpg',
+      price: '120',
+      instock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+    },
+
+    {
+      name: 'Adidas Slim Shirt',
+      slug: 'Adidas-slim-hirt',
+      category: 'shirts',
+      image: '/images/p3.jpg',
+      price: '120',
+      instock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+    },
+
+    {
+      name: 'Adidas Slim Shirt',
+      slug: 'Adidas-slim-hirt',
+      category: 'shirts',
+      image: '/images/p4.jpg',
+      price: '120',
+      instock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+    },
+  ],
+};
+
+export default data;
